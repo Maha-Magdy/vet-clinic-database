cb4b216208c3ee4d5b8cc0e980cca78da47d9d97
German Renato Cobian <germancobian@hotmail.com>
2022-03-30 15:50:26 -0700

    Optimize performance on first query of the project

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 SELECT COUNT(*) FROM visits where animal_id = 4;
 -- to optimize the time execuation and performance for the last query we used the follwing query
-
+CREATE INDEX animal_id_asc ON visits(animal_id ASC);
 
 SELECT * FROM visits where vet_id = 2;
 -- to optimize the time execuation and performance for the last query we used the follwing query
