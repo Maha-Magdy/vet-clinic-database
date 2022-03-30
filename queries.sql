c4e35aee63b5ae5f96f77871f77151e5a9be6165
Maha Magdy <mahamagdy3@gmail.com>
2022-03-31 00:42:17 +0200

    Adding the optimized screenshots, and the the optimizer query for the second query

--- queries.sql
+++ queries.sql
@@ -1,5 +1,10 @@
 SELECT COUNT(*) FROM visits where animal_id = 4;
+-- to optimize the time execuation and performance for the last query we used the follwing query
+
 
 SELECT * FROM visits where vet_id = 2;
+-- to optimize the time execuation and performance for the last query we used the follwing query
+CREATE INDEX vet_id_asc ON visits(vet_id ASC);
 
 SELECT * FROM owners where email = 'owner_18327@mail.com';
+-- to optimize the time execuation and performance for the last query we used the follwing query
